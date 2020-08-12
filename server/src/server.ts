27587f0dc717044db1d8658c066a45919dd387c7
thiago <tcaa@icomp.ufam.edu.br>
2020-08-12 11:53:06 -0400

    feat: variable port name

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,12 @@ var restify = require("restify")
 
 var server = restify.createServer()
 
+const port = process.env.PORT || 8080
+
 server.get("/:name", (req, res, next) => {
     res.send("Hello " + req.params.name)
 })
 
-server.listen(4444, () => {
+server.listen(port, () => {
     console.log(`${server.name} listening at ${server.url}`)
 })
