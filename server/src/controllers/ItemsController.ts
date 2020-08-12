07c51b623ffd60091218ee7e6336144c98b3324f
thiago <tcaa@icomp.ufam.edu.br>
2020-08-12 16:14:05 -0400

    feat: items crud

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -1,10 +1,29 @@
 import { Request, Response, Next } from "restify"
-import Items from "../db/models/items"
+import Item from "../db/models/items"
+import { isRegExp } from "util"
 
 class ItemController {
-    create(req: Request, res: Response, next: Next) {}
+    create(req: Request, res: Response, next: Next) {
+        var newItem = new Item(req.body)
+        newItem.save((err) => {
+            if (err) {
+                res.status(400)
+                res.send("Unable to save shark to database: " + err)
+            } else {
+                res.json(newItem)
+            }
+        })
+        return res
+    }
+
     list(req: Request, res: Response, next: Next) {
-        return res.send("Hello World")
+        Item.find({}).exec((err, sharks) => {
+            if (err) {
+                return res.send(500, err)
+            }
+            res.json(sharks)
+        })
+        return res
     }
 }
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,26 @@
 import * as restify from "restify"
-import ItemController from "./controllers/ItemsController"
-
 const server = restify.createServer()
+
+import ItemController from "./controllers/ItemsController"
 const itemController = new ItemController()
 
 const db = require("./db/connection")
 
 const port = process.env.PORT || 8080
 
+const check = (
+    req: restify.Request,
+    res: restify.Response,
+    next: restify.Next
+) => {
+    res.send("I'm working")
+}
+
+server.use(restify.plugins.bodyParser())
+
+server.get("/", check)
 server.get("/items", itemController.list)
+server.post("/items", itemController.create)
 
 server.listen(port, () => {
     console.log(`${server.name} listening at ${server.url}`)
