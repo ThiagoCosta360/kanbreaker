ccce57acdee41705b78914f39cf80a5841ba5e93
thiago <tcaa@icomp.ufam.edu.br>
2020-08-16 15:33:35 -0400

    WIP: consuming api

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,22 @@
 import * as restify from "restify"
+import * as corsMiddleware from "restify-cors-middleware"
 const server = restify.createServer()
 
 import ItemController from "./controllers/ItemsController"
 const itemController = new ItemController()
 
+//TODO: this need to be loaded without require
 const db = require("./db/connection")
 
 const port = 4000
+
+// Cors config
+const corsOptions: corsMiddleware.Options = corsMiddleware({
+    preflightMaxAge: 10,
+    origins: ["*"],
+    allowHeaders: ["authorization"],
+    exposeHeaders: ["x-custom-header"],
+})
 
 const check = (
     req: restify.Request,
@@ -16,6 +26,8 @@ const check = (
     res.send("I'm working now")
 }
 
+// server.pre(corsOptions.preflight)
+server.use(corsOptions.actual)
 server.use(restify.plugins.bodyParser())
 
 server.get("/", check)
